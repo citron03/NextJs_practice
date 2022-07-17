83d86ab7c25c45f1299005b7657a93eef6b5791d
citron03 <cchan0617@gmail.com>
2022-07-17 21:22:36 +0900

    feat: 스타일 적용 테스트

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,9 @@
 import type {NextPage} from 'next'
+import styles from './../styles/About.module.css';
 
 const About: NextPage = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>테스트 페이지입니다.</h1>
             <h2>Next JS 라우팅 체크</h2>
         </div>

1d42541307d130b6da3dc4ec0738844a78faf00b
citron03 <cchan0617@gmail.com>
2022-07-17 21:11:54 +0900

    feat: 파일 시스템 라우팅 확인

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type {NextPage} from 'next'
+
+const About: NextPage = () => {
+    return (
+        <div>
+            <h1>테스트 페이지입니다.</h1>
+            <h2>Next JS 라우팅 체크</h2>
+        </div>
+    )
+}
+
+export default About
